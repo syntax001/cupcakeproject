ede903b7de5b2d334295d1b3b18e8ccf5550f577
pl0xi <puixdk@gmail.com>
2021-04-23 15:41:48 +0200

    Oprettet AdminFacade class, tilføjet en metode der henter getUsers fra adminMapper.getAllUsers()

---
+++ src/main/java/business/services/AdminFacade.java
@@ -0,0 +1,22 @@
+package business.services;
+
+import business.entities.User;
+import business.exceptions.UserException;
+import business.persistence.AdminMapper;
+import business.persistence.Database;
+
+import java.util.List;
+
+public class AdminFacade {
+    private AdminMapper adminMapper;
+
+    public AdminFacade(Database database) {
+        adminMapper = new AdminMapper(database);
+    }
+
+    public List<User> getUsers() throws UserException {
+        List<User> users = adminMapper.getAllUsers();
+
+        return users;
+    }
+}
